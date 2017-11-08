4df7af8f18c1ec181b263cc8cc274911ddd21e86
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-08 23:10:08 +0000

    update user routes. need to set up database to continue.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,5 +9,12 @@ class UserController < ApplicationController
     #creates an instance of the User class
     #saves to database
     #redirects either Valid? => UserHomePage NotValid => SignUp + flash Message
+     if params[:username] == "" || params[:email] == "" || params[:password] == ""
+       redirect '/signup'
+     else
+       @user = User.create(username: params[:username], email: params[:email], password: params[:password])
+       #     session[:user_id] = @user.id
+       #     redirect '/user_homepage'
+     end
   end
 end
