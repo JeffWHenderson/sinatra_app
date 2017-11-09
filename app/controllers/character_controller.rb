8dd7a599ae6084c1d9cc5a5a13d17aad34dde8e9
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-09 01:36:12 +0000

    added initial routes for character pages.

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,6 +1,26 @@
 class CharacterController < ApplicationController
-  # ///////// CREATE ///////////
-    # ///////// READ ///////////
-      # ///////// UPDATE ///////////
-        # ///////// DESTROY ///////////
+
+      get '/characters' do
+        erb :"/character/characters"
+      end
+
+      # ///////// CREATE ///////////
+      get '/characters/new' do
+        erb :'/character/create_character'
+      end
+
+        # ///////// READ ///////////
+      get '/characters/:id' do
+        erb :'/character/show_character'
+      end
+
+          # ///////// UPDATE ///////////
+      get '/characters/:id/update' do
+        erb :'/character/edit_character'
+      end
+
+            # ///////// DESTROY ///////////
+      delete '/characters/:id/delete' do
+       "The last thing to do i think!!!"
+      end
 end
