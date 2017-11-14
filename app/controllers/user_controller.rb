bdccacddef4bb9744647209307544654e2152b3b
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-14 16:15:46 +0000

    got elsif block to hit in signup page. commented where i need to add flash messages.

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -9,14 +9,14 @@ class UserController < ApplicationController
   end
 
   post '/signup' do
-    # //// add flash messages
-    # //// elsif block isn't getting hit yet
+    #                 ////////////// add flash messages ////////////////////////
+     @user = User.find_by(username: params[:username])
      if params[:username] == "" || params[:email] == "" || params[:password] == ""
        "flash message you can't have a blank field"
-       redirect '/signup'
-     elsif params[:username] == User.all.select { |n| n.username }
+       #redirect '/signup'
+     elsif params[:username] == @user.username
        "flash message that username is taken"
-       redirect '/signup'
+        #redirect '/signup'
      else
        @user = User.create(username: params[:username], email: params[:email], password: params[:password])
        session[:user_id] = @user.id
@@ -25,7 +25,7 @@ class UserController < ApplicationController
   end
 
   get '/user/homepage' do
-    # ///////// add flash message and redirect to login
+    # ///////// add flash message and redirect to login  ///////////////////
     if logged_in?
       @user = User.find_by(:id => session[:user_id])
       erb :'user/user_homepage'
